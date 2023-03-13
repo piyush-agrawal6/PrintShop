96fa8750e2219a91a1e6bdb5689ee346e02a31c7
piyush-agrawal6 <agrawaljoy1@gmail.com>
2023-03-13 10:03:50 +0530

    home page craeted

--- src/Components/product/ProComp.jsx
+++ src/Components/product/ProComp.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "./ProComp.css";
-import { RiStarSFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 const ProComp = ({ product }) => {
   const {
     images,
-    rating,
-    count,
     brand,
-    title,
     price,
     off_price,
     discount,
@@ -19,13 +15,9 @@ const ProComp = ({ product }) => {
       <Link color="a" to={`/product/${id}`}>
         <div className="itemImage">
           <img src={images.image1} alt="ProductImage" />
-          <p>
-            {rating} <RiStarSFill className="itemStars" /> | {count}
-          </p>
         </div>
         <div className="itemDetails">
           <h4>{brand}</h4>
-          <p>{title}</p>
           <div>
             Rs. {price} <s>Rs. {off_price}</s> <span>({discount}% OFF)</span>
           </div>
